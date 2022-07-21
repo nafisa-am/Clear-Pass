2806c75fd1217a65da9a9ff4d1c8665160ebadef
Nafisa Mohamed <nafisa@Nafisas-MacBook-Air.local>
2022-07-21 23:53:53 +0100

    making changes to code and adding Readme

--- Develop/script.js
+++ Develop/script.js
@@ -9,24 +9,177 @@ function writePassword() {
   passwordText.value = password;
 }
 
+//Criteria
+var lowerCaseLetters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var upperCaseLetters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+var specialCharacter = [
+  " ",
+  "!",
+  "#",
+  "$",
+  "%",
+  "&",
+  "(",
+  ")",
+  "*",
+  "+",
+  ",",
+  "-",
+  ".",
+  "/",
+  ":",
+  ";",
+  "<",
+  "=",
+  ">",
+  "?",
+  "@",
+  "[",
+  "]",
+  "^",
+  "_",
+  "`",
+  "{",
+  "|",
+  "}",
+  "~",
+  " ' ",
+  " ",
+  ' " ',
+  "\\",
+];
+
+var userChoice = "";
+var finalPwd;
+var length = 0;
+
+// Initial Prompts
+var lowerCaseLetters = confirm(
+  "A strong password should include upper case letters."
+);
+var upperCaseLetters = confirm(
+  "A strong password should include lower case letters."
+);
+var numbers = confirm("A strong password should include numbers.");
+var specialCharacter = confirm(
+  "A strong password should include special characters."
+);
+
 // Main Function to generate password
+function generatePassword() {
+  var length = prompt(
+    "How long would you like your password to be? Please note, it must be between 8 - 128 characters"
+  );
+  if (length < 8 || length > 128) {
+    alert("Password must be between 8 - 128!");
+    return generatePassword();
+  }
+  var lowerCaseLetters = confirm(
+    "Would you like to include lower case characters in your password?"
+  );
+  if (lowerCaseLetters == true) {
+    userChoice = userChoice.concat(lowerCaseLetters);
+    console.log(userChoice);
+  }
 
-var upperCase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
-var lowerCase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
-var numbers = "0,1,2,3,4,5,6,7,8,9";
-var specialCharacter =
-  " ,!,\",#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,],^,_,`,{,|,},~,";
+  var upperCaseLetters = confirm(
+    "Would you like to include upper case characters in your password?"
+  );
+  if (upperCaseLetters == true) {
+    userChoice = userChoice.concat(upperCaseLetters);
+    console.log(userChoice);
+  }
 
-var generatePassword = () => {};
-// Get password length
-var passwordLength = getPasswordLength();
-{
-  return 12;
+  var specialCharacter = confirm(
+    "Would you like to include special characters in your password?"
+  );
+  if (specialCharacter == true) {
+    userChoice = userChoice.concat(specialCharacter);
+    console.log(userChoice);
+  }
+
+  var number = confirm("Would you like to include numbers in your password?");
+  if (number == true) {
+    userChoice = userChoice.concat(number);
+    console.log(userChoice);
+  }
+
+  // Option if neither of the minimum password requirements are selected
+  if (
+    lowerCaseLetters === false &&
+    upperCaseLetters === false &&
+    specialCharacter === false &&
+    number === false
+  ) {
+    prompt(
+      "Please note, the password must include at least one of the following: an upper case letter, a lower case letter, special character or number in order to be valid"
+    );
+  }
+
+  var charAt = finalPwd;
+  console.log(charAt);
+
+  for (var i = 0; i < length; i++) {
+    result += finalPwd.charAt(Math.floor(Math.random() * finalPwd.length));
+  }
+
+  return result;
 }
-// Get password criteria
-var passwordCriteria = getPasswordCriteria();
-// Create random password
-var password = createRandomPassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # Clear-Pass
+
+# Application used HTML, CSS, Javascript
+
+# Brief Description:
+
+For this this project I was tasked with modifying a starter code to create an application that enables employees to generate random passwords based on criteria that they’ve selected.
+
+This app will run in the browser and will feature dynamically updated HTML and CSS powered by JavaScript code that I wrote. It will have a clean and polished, responsive user interface.
+
+# Screenshots
+
+# Links
+
+Deployed Link
+Github link -
