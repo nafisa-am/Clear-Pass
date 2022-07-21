a7a3e816971a905c7adf9f8428b43f51930dd61d
Nafisa Mohamed <nafisa@Nafisas-MacBook-Air.local>
2022-07-21 14:19:02 +0100

    adding code

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Main Function to return password
-
-var generatePassword = () => {
-  return "hb89IKlw@sp1!";
-};
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -14,6 +8,25 @@ function writePassword() {
 
   passwordText.value = password;
 }
+
+// Main Function to generate password
+
+var upperCase = "A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z";
+var lowerCase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+var numbers = "0,1,2,3,4,5,6,7,8,9";
+var specialCharacter =
+  " ,!,\",#,$,%,&,',(,),*,+,,,-,.,/,:,;,<,=,>,?,@,[,],^,_,`,{,|,},~,";
+
+var generatePassword = () => {};
+// Get password length
+var passwordLength = getPasswordLength();
+{
+  return 12;
+}
+// Get password criteria
+var passwordCriteria = getPasswordCriteria();
+// Create random password
+var password = createRandomPassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
